a370709c97d99f9329a72ed8ebdba65b608af9df
Zaid <itsunknown122@gmail.com>
2023-06-08 17:33:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',

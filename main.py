627abc229d4513a0cbbb2c990fdd350ce4226523
Zaid <itsunknown122@gmail.com>
2023-06-08 17:21:59 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Configure logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Telegram bot token
+TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN'
+
+# Command handler for the start command
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a welcome message when the command /start is issued."""
+    update.message.reply_text('Welcome! Send me a Telegram file to generate a streaming link.')
+
+# Command handler for file message
+def file_message(update: Update, context: CallbackContext) -> None:
+    """Handle incoming file message."""
+    file = update.message.document  # Get the received file object
+    file_id = file.file_id  # Get the file ID
+
+    # Generate streaming link
+    streaming_link = context.bot.get_file(file_id).file_path
+
+    # Share the streaming link
+    update.message.reply_text(f"Streaming link: {streaming_link}")
+
+# Main function
+def main() -> None:
+    """Run the bot."""
+    # Create the Updater and pass it the bot token
+    updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # Register command handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.document, file_message))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
